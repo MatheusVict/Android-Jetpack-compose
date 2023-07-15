0c933ac578508188e03c88e647461c565a8e536a
MatheusVict <matheusvictorhenrique@gmail.com>
2023-07-15 16:51:12 -0300

    Row

--- app/src/main/java/com/matheusvict/firstcompose/MainActivity.kt
+++ app/src/main/java/com/matheusvict/firstcompose/MainActivity.kt
@@ -3,14 +3,25 @@ package com.matheusvict.firstcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.matheusvict.firstcompose.ui.theme.FirstComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,20 +37,29 @@ class MainActivity : ComponentActivity() {
 
     @Composable
     private fun FirstScreen() {
-        Column() {
-            Text(
-                text = "Hello world"
-            )
-            Text(
-                text = "Matheus"
-            )
-            Text(
-                text = "Matheus"
-            )
+
+        Row(
+            modifier = Modifier.fillMaxWidth().fillMaxHeight(),
+            Arrangement.Center,
+        ) {
+            Surface(
+                color = Color.Red,
+                modifier = Modifier.width(100.dp).height(100.dp)
+            ) {}
+
+            Surface(
+                color = Color.Blue,
+                modifier = Modifier.width(100.dp).height(100.dp)
+            ) {}
+
+            Surface(
+                color = Color.Black,
+                modifier = Modifier.width(100.dp).height(100.dp)
+            ) {}
         }
 
     }
-    
+
     @Preview
     @Composable
     private fun FirstScreenPreview() {
