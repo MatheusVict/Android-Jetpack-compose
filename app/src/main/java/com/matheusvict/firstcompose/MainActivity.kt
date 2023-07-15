bd9115615fff12c56b3d5155d0deec9c08f23d4a
MatheusVict <matheusvictorhenrique@gmail.com>
2023-07-15 13:23:51 -0300

    initial commit

---
+++ app/src/main/java/com/matheusvict/firstcompose/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.matheusvict.firstcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.matheusvict.firstcompose.ui.theme.FirstComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            FirstComposeTheme {
+
+
+            }
+        }
+    }
+}
+
